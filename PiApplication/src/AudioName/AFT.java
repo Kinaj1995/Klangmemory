f314f06d38c3314bbe7d945bc2702246241f3702
Adi <a_birrer85@hotmail.com>
2022-03-09 23:24:05 +0100

    Textausgabe für Audiodateien benannt

--- PiApplication/src/AudioName/AFT.java
+++ PiApplication/src/AudioName/AFT.java
@@ -29,48 +29,48 @@ public class AFT {
         switch (category) {
 
             case "Kat.1":
-                card1 = "Brawo, du hast die Katze gefunden";
-                card2 = "Brawo, du hast den Hund gefunden";
-                card3 = "";
-                card4 = "";
-                card5 = "";
-                card6 = "";
-                card7 = "";
-                card8 = "";
-                card9 = "";
-                card10 = "";
-                card11 = "";
-                card12 = "";
+                card1 = "Bravo, du hast die Katze gefunden";
+                card2 = "Bravo, du hast den Hund gefunden";
+                card3 = "Bravo, du hast den Löwe gefunden";
+                card4 = "Bravo, du hast das Pferd gefunden";
+                card5 = "Bravo, du hast die Ente gefunden";
+                card6 = "Bravo, du hast den Esel gefunden";
+                card7 = "Bravo, du hast das Schwein gefunden";
+                card8 = "Bravo, du hast das Schaf gefunden";
+                card9 = "Brawo, du hast die Hühner gefunden";
+                card10 = "Bravo, du hast die Schlange gefunden";
+                card11 = "Bravo, du hast die Kuh gefunden";
+                card12 = "Bravo, du hast den Affe gefunden";
                 break;
 
             case "Kat.2":
-                card1 = "Brawo, du hast den Helikopter gefunden";
-                card2 = "Brawo, du hast das Tram gefunden";
-                card3 = "";
-                card4 = "";
-                card5 = "";
-                card6 = "";
-                card7 = "";
-                card8 = "";
-                card9 = "";
-                card10 = "";
-                card11 = "";
-                card12 = "";
+                card1 = "Bravo, du hast den Helikopter gefunden";
+                card2 = "Bravo, du hast das Tram gefunden";
+                card3 = "Bravo, du hast das Flugzeug gefunden";
+                card4 = "Bravo, du hast das Motorrad gefunden";
+                card5 = "Bravo, du hast das Auto gefunden";
+                card6 = "Bravo, du hast den Traktor gefunden";
+                card7 = "Bravo, du hast die Polizei gefunden";
+                card8 = "Bravo, du hast den Lastwagen gefunden";
+                card9 = "Bravo, du hast die Feuerwehr gefunden";
+                card10 = "Bravo, du hast das Postauto gefunden";
+                card11 = "Bravo, du hast das Dampfschiff gefunden";
+                card12 = "Bravo, du hast das Velo gefunden";
                 break;
 
             case "Kat.3":
-                card1 = "Brawo, du hast das WC gefunden";
-                card2 = "Brawo, du hast den Wecker gefunden";
-                card3 = "";
-                card4 = "";
-                card5 = "";
-                card6 = "";
-                card7 = "";
-                card8 = "";
-                card9 = "";
-                card10 = "";
-                card11 = "";
-                card12 = "";
+                card1 = "Bravo, du hast die WC Spühlung gefunden";
+                card2 = "Bravo, du hast den Wecker gefunden";
+                card3 = "Bravo, du hast das Tropfen gefunden";
+                card4 = "Bravo, du hast das alte Telefon gefunden";
+                card5 = "Bravo, du hast die Motorsäge gefunden";
+                card6 = "Bravo, du hast den Wind gefunden";
+                card7 = "Bravo, du hast den Donner gefunden";
+                card8 = "Bravo, du hast die Kirchturmglocke gefunden";
+                card9 = "Bravo, du hast den tropfenden Wasserhahn gefunden";
+                card10 = "Bravo, du hast die Kaffeemaschine gefunden";
+                card11 = "Bravo, du hast die Fliege gefunden";
+                card12 = "Bravo, du hast die Waschmaschine gefunden";
                 break;
 
             default:
